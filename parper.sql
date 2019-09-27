9ac4c96d5e7327ebd8143de1a8dc4f3ea6e3519d
“zzx <546397335@qq.com>
2019-09-27 17:59:46 +0800

    27/09/2019

---
+++ parper.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50714
+Source Host           : localhost:3306
+Source Database       : parper
+
+Target Server Type    : MYSQL
+Target Server Version : 50714
+File Encoding         : 65001
+
+Date: 2019-09-27 17:47:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
